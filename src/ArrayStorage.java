a4b4f15912b558cedcaff52de2ed05af5481fe85
Davyd Homaz <homazdavid99@gmail.com>
2025-05-08 16:21:23 +0200

    doing homework in an ArrayStorage file

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -1,30 +1,70 @@
+import java.util.Arrays;
+import java.util.Objects;
+
 /**
  * Array based storage for Resumes
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
+    int count = 0;
 
     void clear() {
+        for (int i = 0; i < storage.length - 1; i++) {
+            delete(String.valueOf(storage[i]));
+        }
     }
 
     void save(Resume r) {
+        if (count < storage.length) {
+            storage[count] = r;
+            count++;
+        } else {
+            System.out.println("Массив заполнен, не удалось добавить резюме: " + r);
+        }
     }
 
     Resume get(String uuid) {
-        return null;
+        Resume result = null;
+        for (Resume str : storage) {
+            if (str != null && str.uuid.equals(uuid)) {
+                result = str;
+            }
+        }
+        return result;
     }
 
     void delete(String uuid) {
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null && storage[i].uuid.equals(uuid)) {
+                storage[i] = null;
+            }
+        }
+        for (int i = 1; i < storage.length - 1; i++) {
+            storage[i - 1] = storage[i];
+            storage[i] = null;
+        }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
     Resume[] getAll() {
-        return new Resume[0];
+        Resume[] result = new Resume[10000];
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                result[i] = storage[i];
+            }
+        }
+        return result;
     }
 
     int size() {
-        return 0;
+        int result = 0;
+        for (int i = 0; i < storage.length - 1; i++) {
+            if (storage[i] != null) {
+                result++;
+            }
+        }
+        return result;
     }
 }
