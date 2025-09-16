fa6218e956ff80336807e1d8a746fed2b3803120
David <homazdavid99@Gmail.com>
2025-09-16 14:56:20 +0300

    error correction

--- src/com/unise/webapp/JavaStreams.java
+++ src/com/unise/webapp/JavaStreams.java
@@ -1,39 +1,27 @@
 package com.unise.webapp;
 
 import java.util.ArrayList;
-import java.util.HashSet;
+import java.util.Arrays;
 import java.util.List;
 
 public class JavaStreams {
-    static int[] array = {9, 8};
+    static int[] array = {1, 2, 3, 3, 2, 3};
     //int result = minValue(array);
 
     private static int minValue(int[] values) {
-        HashSet<Integer> set = new HashSet<>();
-        int n = 0;
-        for (int i : values) {
-            set.add(i);
-        }
-        for (int result : set) {
-            n = (10 * n) + result;
-        }
-        return n;
+        return (int) Arrays.stream(values).asLongStream().distinct().sorted()
+                .reduce(0, (result, i) -> 10 * result + i);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        int number = integers.stream().mapToInt(Integer::intValue).sum();
-        if (number % 2 == 0) {
-            integers.removeIf(i -> i % 2 == 0);
-        } else {
-            integers.removeIf(i -> i % 2 == 1);
-        }
-        return integers;
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        return integers.stream().filter(i -> (sum % 2 == 0) == (i % 2 != 0)).toList();
     }
 
     public static void main(String[] args) {
         List<Integer> list = new ArrayList<>();
 
-        for (int i = 0; i < 13; i++) {
+        for (int i = 0; i < 14; i++) {
             list.add(i);
         }
 
